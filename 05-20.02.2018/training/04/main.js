44973bce10d0aeb0541e61dfc5bb195d32010f61
MiF <grigorchuk.konstantin@gmail.com>
2018-02-20 19:00:37 +0300

    training-05

--- 05-20.02.2018/training/04/main.js
+++ 05-20.02.2018/training/04/main.js
@@ -1,3 +1,36 @@
+// Плюсы: можем насоздавать много сторов, экшены для действий над данными, модель в каком то виде
+// Минусы: сложно понять и принять концепцию экшенов и редъюсеров, пного писанины с AC и редъюсерами
+
+
+class Store {
+
+    constructor(defState){
+        this.state = defState
+    }
+
+    addElement(id, text, done = false) {
+        this.state = Object.assign({}, this.state)
+        this.state.list = this.state.list.slice()
+        this.state.list.push({ id, text, done })
+    }
+
+    getState(){
+        return this.state
+    }
+}
+
+const store = new Store({
+    list: [
+        { id: 1, text: 'Yo!', done: false },
+        { id: 2, text: 'Vasiliy', done: false },
+    ]
+})
+
+store.addElement(3, 'Vasiliy3')
+
+console.log(JSON.stringify(store.getState(), null, 4))
+
+
 const ADD_ITEM = 'ADD_ITEM'
 
 class Store {
